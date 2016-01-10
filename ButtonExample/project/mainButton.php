8bff2c5abc6e8cecc2bf8ba9461d5bd55ded08f8
pablanco <pblanco@devxtend.com>
2016-01-10 14:36:54 -0300

    Button main example update

--- ButtonExample/project/mainButton.php
+++ ButtonExample/project/mainButton.php
@@ -12,13 +12,14 @@ $win->setCaption("Button control");
 $tabMain = new Table();
  
 $button = new Button();
-$button->setCaption("Save");
-$button->onTap(save());
+$button->setCaption("Button");
+$button->setClass("button.Example");
+$button->onTap(action());
  
 $tabMain->addControl($button,1,1);
 $win->addControl($tabMain);
  
-function save(){
+function action(){
 	echo "Hello world!";
 }
  
