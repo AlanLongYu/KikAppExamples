3a7ddc89f583d22c2d0b6defebd71d93302e0f93
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-02-22 11:38:29 -0300

    Examples update

--- GridExample/project/main.php
+++ GridExample/project/main.php
@@ -6,19 +6,82 @@
  */
 
 $win = new SDPanel();
-$win -> setCaption("Hello world!");
+$win -> setCaption("");
 
-$mainTable = new Table();
+$table = new Table();
+$table -> setClass("tableGray");
 
-$button = new Button();
-$button -> setCaption("Click me!");
-$button -> onTap(clickme());
+$grid = new Grid();
+$table_grid_product = new Table();
+$table_grid_product -> setClass("tableGrid");
+$table_grid_product -> setRowsStyle("80dip");
+$table_grid_product -> setColumnsStyle("35%;65%;15dip");
+$table_grid_product -> setHeight("85dip");
 
-$mainTable -> addControl($button,1,1);
-$win -> addControl($mainTable);
+$grid -> addData(grid_load_products());
+$grid -> onTap(action_prod());
 
-function clickme(){
-	echo "Hello !";
+//inputs and controls
+$title_prod 	= new InputText(150);
+$title_prod -> setClass("attributetitleList");
+$title_prod -> setAutoGrow(true);
+$image_prod 	= new InputImage();
+$image_prod -> setClass("imageImageList2");
+$desc_prod 	= new InputText(300);
+$desc_prod -> setClass("attributeTextGray");
+$price_prod 	= new InputText(10);
+$price_prod -> setClass("attributeTextPrice");
+
+$table_desc_prod = new Table();
+$table_desc_prod -> setRowsStyle("25dip;35dip;25dip");
+$table_desc_prod -> setClass("tableProduct");
+$table_desc_prod -> addControl($title_prod,1,1);
+$table_desc_prod -> addControl($desc_prod,2,1);
+$table_desc_prod -> addControl($price_prod,3,1);
+
+$table_grid_product -> addControl($image_prod,1,1);
+$table_grid_product -> addControl($table_desc_prod,1,2);
+
+$grid -> addControl($table_grid_product);
+
+$table -> setRowsStyle("100%");
+$table -> addControl($grid,1,1);
+
+$win -> addControl($table);
+
+
+
+function action_prod(){
+	$win -> Open("product_detail",$id_prod,$title_prod,$desc_prod,$price_prod);
+}
+
+function grid_load_products(){
+	$url_prod = "http://www.demo.kikapptools.com/magento/apiGecko/productos.php?cId=0";
+	$httpClient_prod = new httpClient();
+
+	$result_prod = $httpClient_prod -> Execute('GET',$url_prod);
+
+	$struct_prod = array(
+			array(
+					"id" => DataType::Numeric(6),
+					"name" => DataType::Character(150),
+					"description" => DataType::Character(300),
+					"pirce" => DataType::Character(10),
+					"thumb"=> DataType::Character(200)
+			)
+	);
+
+	Data::FromJson($struct_prod,$result_prod);
+
+	foreach ($struct_prod as $product){
+		$id_prod 	= $product['id'];
+		$title_prod 	= $product['name'];
+		$desc_prod 	= $product['description'];
+		$image_prod 	= $product['thumb'];
+		$price_prod 	= $product['pirce'];
+	}
+	
+	$id_prod = new InputNumeric();
 }
 
 ?>
