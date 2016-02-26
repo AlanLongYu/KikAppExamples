b74e0caf2117c63d2f78ba79befebfa21d2fc424
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-02-26 13:43:58 -0300

    Examples update

--- ImageGalleryExample/project/main.php
+++ ImageGalleryExample/project/main.php
@@ -1,24 +1,58 @@
 <?php 
 /**
- * Main object.
- * @author Kikapp
+ * Example Image Gallery
+ * @author KikApp
  * @version 1.0
  */
 
 $win = new SDPanel();
-$win -> setCaption("Hello world!");
+$win -> setCaption("Image Gallery");
 
 $mainTable = new Table();
 
-$button = new Button();
-$button -> setCaption("Click me!");
-$button -> onTap(clickme());
+//Create Inputs
+$id = new InputNumeric();
+$title = new InputText();
+$desc = new InputText();
+$image = new InputImage();
 
-$mainTable -> addControl($button,1,1);
+//Create Image Gallery
+$grid = new ImageGallery();
+$grid -> setImage($image);
+$grid -> setTitle($title);
+$grid -> setSubtitle($desc);
+$grid -> setEnableShare(true);
+
+$grid -> addData(load_images());
+
+$mainTable -> addControl($grid,1,1);
 $win -> addControl($mainTable);
 
-function clickme(){
-	echo "Hello !";
+function load_images(){	
+	$url = "http://demo.kikapptools.com/magento/apiGecko/productos.php?cId=0";
+	$httpClient = new httpClient();
+	
+	$result = $httpClient -> Execute('GET',$url);
+	
+	$struct = array(
+			array(
+					"id" => DataType::Numeric(6),
+					"name" => DataType::Character(150),
+					"description" => DataType::Character(300),
+					"pirce" => DataType::Character(10),
+					"thumb"=>DataType::Character(200)
+			)
+	);
+	
+	Data::FromJson($struct,$result);
+			
+	foreach ($struct as $product){
+		$id 	= $product['id'];
+		$title 	= $product['name'];
+		$desc 	= $product['description'];
+		$image 	= $product['thumb'];
+	}
+		
 }
 
 ?>
