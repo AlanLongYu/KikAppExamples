60eb3feb7ead2d5c3b619d264ee383b7d4525db4
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-08-02 12:22:42 -0300

    Examples update

--- InteropMessage/project/main.php
+++ InteropMessage/project/main.php
@@ -9,21 +9,25 @@ $win = new SDPanel();
 $win -> setCaption("Interop");
 
 $actionBar = new ActionBar();
-$actionBar->setClass("applicationBars");
+$actionBar ->setClass("applicationBars");
 
 $mainTable = new Table();
 
-$input_message= new InputText();
-$input_message-> setLabelCaption("Write the message");
+$input_message = new InputText();
+$input_message -> setLabelCaption("Write the message");
+
+$input_phone = new InputPhone();
+$input_phone -> setLabelCaption("Write the phone number");
 
 $button_message= new Button();
 $button_message-> setCaption("Message!");
 $button_message-> onTap(message());
 
-$mainTable-> addControl($input_message,1,1);
-$mainTable-> addControl($button_message,2,1);
+$mainTable -> addControl($input_message,1,1);
+$mainTable -> addControl($input_phone,2,1);
+$mainTable -> addControl($button_message,3,1);
 
-$win-> addControl($mainTable);
+$win -> addControl($mainTable);
 
 function message() {
    Interop::SendMessage($input_message,$input_phone);
