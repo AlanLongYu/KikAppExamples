56fb31b0638b7d10df18e0da69937b6cac2d32aa
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-08-04 11:45:29 -0300

    Examples update

--- CheckBoxExample/project/main.php
+++ CheckBoxExample/project/main.php
@@ -24,30 +24,12 @@ $check_box1-> setCheckValue("Check Box 1 checked!");
 $check_box1-> setUnCheckValue("Check Box 1 unchecked!");
 
 
-$check_box2= new CheckBox();
-$check_box2-> setLabelCaption("Check Box 2");
-$check_box2-> setLabelPosition("Top");
-$check_box2-> setCheckValue("Check Box 2 checked!");
-$check_box2-> setUnCheckValue("Check Box 2 unchecked!");
-
-
-$check_box3= new CheckBox();
-$check_box3-> setLabelCaption("Check Box 3");
-$check_box3-> setLabelPosition("Top");
-$check_box3-> setCheckValue("Check Box 3 checked!");
-$check_box3-> setUnCheckValue("Check Box 3 unchecked!");
-
-
 $mainTable-> addControl($check_box1,1,1);
-$mainTable-> addControl($check_box2,2,1);
-$mainTable-> addControl($check_box3,3,1);
-$mainTable -> addControl($button,4,1);
+$mainTable -> addControl($button,2,1);
 $win -> addControl($mainTable);
 
 function clickme(){
-	echo "".$check_box1->getValue();
-	echo "".$check_box2->getValue();
-	echo "".$check_box3->getValue();
+   echo $check_box1->getValue();
 }
 
 ?>
