2918fc57041cb0f786525f65bf5a460c7cf8f8f3
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-10-04 11:19:12 -0300

    Examples update

---
+++ InteropConfirm/project/main.php
@@ -0,0 +1,28 @@
+<?php 
+/**
+ * Main object.
+ * @author Kikapp
+ * @version 1.0
+ */
+
+$win = new SDPanel();
+$win -> setCaption("Hello world!");
+
+$mainTable = new Table();
+
+$button = new Button();
+$button -> setCaption("Click me!");
+$button -> onTap(back());
+
+$mainTable -> addControl($button,1,1);
+$win -> addControl($mainTable);
+
+function back(){
+   $ok = new InputBoolean();
+   $ok = Interop::Confirm('Are you sure you want to close InteropConfirm app?');
+   if($ok == true){
+      return;
+   }
+}
+
+?>
