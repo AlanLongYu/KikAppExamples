bf812076f7f6d49bc9ba9a0424774a7a03d4867d
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-10-24 13:11:14 -0300

    Examples update

--- PopupExample/project/Filtro.php
+++ PopupExample/project/Filtro.php
@@ -10,12 +10,12 @@ $table = new Table();
 $table -> setFormClass("form.algo");
 
 $fecha = new Label();
-$fecha -> setCaption("Fecha");
+$fecha -> setCaption("Date");
 
 $combo_box_fecha= new ComboBox();
 $combo_box_fecha-> setEmptyItem(false);
 //$combo_box_fecha-> setEmptyItemText("Choose options");
-$combo_box_fecha-> setValues("Hoy:1, Proximos 7 dias:2, Proximos 15 dias:3");
+$combo_box_fecha-> setValues("Today:1,Next 7 days:2, Next 15 days:3");
 
 $genero = new Label();
 $genero -> setCaption("Genero");
@@ -23,15 +23,15 @@ $genero -> setCaption("Genero");
 $combo_box_genero= new ComboBox();
 $combo_box_genero-> setEmptyItem(false);
 //$combo_box_fecha-> setEmptyItemText("Choose options");
-$combo_box_genero-> setValues("Rock:1, Cumbia:2, Pop:3");
+$combo_box_genero-> setValues("Rock:1, Pop:2, Opera:3");
 
 $lugar = new Label();
-$lugar -> setCaption("Lugar");
+$lugar -> setCaption("Place");
 
 $combo_box_lugar= new ComboBox();
 $combo_box_lugar-> setEmptyItem(false);
 //$combo_box_fecha-> setEmptyItemText("Choose options");
-$combo_box_lugar-> setValues("Todos:1, Estadio Centenario:2, Velodromo:3");
+$combo_box_lugar-> setValues("All:1, Staples Center:2, Quickens Loans Arena:3");
 
 $boton = new Button();
 $boton -> setCaption("Buscar");
@@ -48,6 +48,6 @@ $table -> addControl($boton,4,1,1,2,"","");
 $win -> addControl($table);
 
 function prueba(){
-   echo "Buscando...";
+   echo "Searching...";
 }
 ?>
