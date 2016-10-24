16579bcab1c446c3cd5cba0e975f3668693cde3e
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-10-24 13:55:28 -0300

    Examples update

--- PopupExample/project/Filtro.php
+++ PopupExample/project/Filtro.php
@@ -9,22 +9,6 @@ $win -> setCaption("");
 $table = new Table();
 $table -> setFormClass("form.algo");
 
-$fecha = new Label();
-$fecha -> setCaption("Date");
-
-$combo_box_fecha= new ComboBox();
-$combo_box_fecha-> setEmptyItem(false);
-//$combo_box_fecha-> setEmptyItemText("Choose options");
-$combo_box_fecha-> setValues("Today:1,Next 7 days:2, Next 15 days:3");
-
-$genero = new Label();
-$genero -> setCaption("Gender");
-
-$combo_box_genero= new ComboBox();
-$combo_box_genero-> setEmptyItem(false);
-//$combo_box_fecha-> setEmptyItemText("Choose options");
-$combo_box_genero-> setValues("Rock:1, Pop:2, Opera:3");
-
 $lugar = new Label();
 $lugar -> setCaption("Place");
 
@@ -37,12 +21,8 @@ $boton = new Button();
 $boton -> setCaption("Search");
 $boton -> onTap(prueba());
 
-$table -> addControl($fecha,1,1);
-$table -> addControl($combo_box_fecha,1,2);
-$table -> addControl($genero,2,1);
-$table -> addControl($combo_box_genero,2,2);
-$table -> addControl($lugar,3,1);
-$table -> addControl($combo_box_lugar,3,2);
+$table -> addControl($lugar,1,1);
+$table -> addControl($combo_box_lugar,1,2);
 $table -> addControl($boton,4,1,1,2,"","");
 
 $win -> addControl($table);
