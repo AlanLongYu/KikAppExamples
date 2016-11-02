36efc0dd2d45ee0e09a8c02656c33baf0c5c46f3
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-11-02 13:11:01 -0300

    Examples update

--- CheckBoxExample/project/main.php
+++ CheckBoxExample/project/main.php
@@ -6,30 +6,24 @@
  */
 
 $win = new SDPanel();
-$win -> setCaption("Check Box!");
+$win -> setCaption("SwitchControl");
 
 $mainTable = new Table();
 
-$actionBar = new ActionBar();
-$actionBar->setClass("applicationBars");
+$switch1 = new SwitchControl();
+$switch1 -> setLabelCaption("Notifications");
+$switch1 -> setLabelPosition("Left");
 
 $button = new Button();
 $button -> setCaption("Click me!");
 $button -> onTap(clickme());
 
-$check_box1= new CheckBox();
-$check_box1-> setLabelCaption("Check Box 1");
-$check_box1-> setLabelPosition("Top");
-$check_box1-> setCheckValue("Check Box 1 checked!");
-$check_box1-> setUnCheckValue("Check Box 1 unchecked!");
-
-
-$mainTable-> addControl($check_box1,1,1);
+$mainTable -> addControl($switch1,1,1,1,1,"Center","Middle");
 $mainTable -> addControl($button,2,1);
 $win -> addControl($mainTable);
 
 function clickme(){
-   echo $check_box1->getValue();
+   echo $switch1->getValue();
 }
 
 ?>
--- CheckBoxExample/project/services/rest/RestClient/HttpClient.class.php
+++ CheckBoxExample/project/services/rest/RestClient/HttpClient.class.php
@@ -2,7 +2,7 @@
 
 /*
  * Copyright 2015 KikApp
- * @version 0.1.3
+ * @version 0.1.5
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
