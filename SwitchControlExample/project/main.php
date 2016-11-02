79189f294f6df8f8d170d0a68712180849a5a3c7
Nicolás Kacowicz <nicolaskaco@hotmail.com>
2016-11-02 12:59:59 -0300

    Examples update

---
+++ SwitchControlExample/project/main.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * Main object.
+ * @author Kikapp
+ * @version 1.0
+ */
+
+$win = new SDPanel();
+$win -> setCaption("Hello world");
+
+$mainTable = new Table();
+
+$switch1 = new SwitchControl();
+$switch1 -> setLabelCaption("Notifications");
+$switch1 -> setLabelPosition("Left");
+
+$button = new Button();
+$button -> setCaption("Click me!");
+$button -> onTap(clickme());
+
+$mainTable -> addControl($switch1,1,1,1,1,"Center","Middle");
+$mainTable -> addControl($button,2,1);
+$win -> addControl($mainTable);
+
+function clickme(){
+   echo $switch1->getValue();
+}
+
+?>
